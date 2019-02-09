6fd2272a7d6433fc172db7322f9c66379cea10b7
Andrea <avertrees@yahoo.com>
2019-02-08 17:01:50 -0700

    friday-5

---
+++ game.js
@@ -0,0 +1,36 @@
+let bear = {
+  name: "Harvey",
+}
+let food = {
+  dumpster: 0,
+  picnic: 0,
+  fish: 0,
+  poison: 0
+}
+
+let health = 0
+
+function slap(val) {
+  health = val + health;
+  document.getElementById("health-meter").innerText = health.toString()
+}
+function ranger(val) {
+  health = val + health;
+  document.getElementById("health-meter").innerText = health.toString()
+
+}
+/*
+var slider = document.getElementById("health-meter");
+var output = document.getElementById("health-meter");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+  output.innerHTML = this.value;
+
+}
+*/
+function rest() {
+  health = 0;
+  document.getElementById("health-meter").innerText = health.toString()
+}
+rest()
